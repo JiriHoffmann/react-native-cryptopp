16174210b107683975f1d225c39defbf0a37c4ec
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2022-02-16 14:09:35 -0500

    feat(ios): lazy JSI install

--- ios/Cryptopp.mm
+++ ios/Cryptopp.mm
@@ -20,41 +20,26 @@ RCT_EXPORT_MODULE()
     return YES;
 }
 
-- (void)setBridge:(RCTBridge *)bridge {
-    _bridge = bridge;
-    _setBridgeOnMainQueue = RCTIsMainQueue();
-    [self installLibrary];
+// Installing JSI Bindings as done by
+// https://github.com/mrousavy/react-native-mmkv
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(install)
+{
+    RCTBridge* bridge = [RCTBridge currentBridge];
+    RCTCxxBridge* cxxBridge = (RCTCxxBridge*)bridge;
+    if (cxxBridge == nil) {
+        return @false;
+    }
+
+    auto jsiRuntime = (jsi::Runtime*) cxxBridge.runtime;
+    if (jsiRuntime == nil) {
+        return @false;
+    }
+
+    rncryptopp::install(*(facebook::jsi::Runtime *)jsiRuntime);
+
+    return @true;
 }
 
-- (void)installLibrary {
-    
-    RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
-    
-    if (!cxxBridge.runtime) {
-        
-        /**
-         * This is a workaround to install library
-         * as soon as runtime becomes available and is
-         * not recommended. If you see random crashes in iOS
-         * global.xxx not found etc. use this.
-         */
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.001 * NSEC_PER_SEC),
-                       dispatch_get_main_queue(), ^{
-            /**
-             When refreshing the app while debugging, the setBridge
-             method is called too soon. The runtime is not ready yet
-             quite often. We need to install library as soon as runtime
-             becomes available.
-             */
-            [self installLibrary];
-            
-        });
-        return;
-    }
-    
-    rncryptopp::install(*(facebook::jsi::Runtime *)cxxBridge.runtime);
-}
 
 - (void)invalidate {
    rncryptopp::cleanup();
