fe1fb627db49268ea62cb3a1ece37e1e257a1457
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2022-02-22 23:23:12 -0500

    feat(JS): Simple example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,85 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import Cryptopp from 'react-native-cryptopp';
+import ImagePicker from 'react-native-image-crop-picker';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function App() {
-  const [result, setResult] = React.useState<string>();
+  const [textInput, setTextInput] = useState('');
+  const [result, setResult] = useState({
+    aes: '',
+    sha: '',
+    sha2: '',
+    sha3: '',
+  });
 
-  React.useEffect(() => {
-    setResult(Cryptopp.SHA.sha1('Hello World'));
-  }, []);
+  useEffect(() => {
+    const key = '12345678901234561234567890123456';
+    const iv = '12345678901234561234567890123456';
+
+    const aes = Cryptopp.AES.encrypt(textInput, key, iv, 'cbc');
+    const sha = Cryptopp.SHA.sha1(textInput);
+    const sha2 = Cryptopp.SHA.sha2(textInput, '512');
+    const sha3 = Cryptopp.SHA.sha3(textInput, '512');
+
+    setResult({
+      aes,
+      sha,
+      sha2,
+      sha3,
+    });
+  }, [textInput]);
+
+  const _openImagePicker = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then((image) => {
+      ReactNativeBlobUtil.fs.readFile(image.path, 'base64').then((data) => {
+        setTextInput(data);
+      });
+    });
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.header}>Input value:</Text>
+      <TextInput
+        value={textInput}
+        onChangeText={setTextInput}
+        style={styles.textInput}
+      />
+
+      <View style={styles.inputButtonsContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setTextInput('')}
+        >
+          <Text style={styles.buttonText}>Clear</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={_openImagePicker}>
+          <Text style={styles.buttonText}>Open image picker</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.header}>Result:</Text>
+      {Object.keys(result).map((key) => (
+        <Text key={key} style={styles.result} numberOfLines={1}>
+          {/* @ts-ignore */}
+          {key}: {result[key]}
+        </Text>
+      ))}
     </View>
   );
 }
@@ -20,12 +87,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: '5%',
+    marginVertical: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  header: {
+    fontSize: 20,
+  },
+  textInput: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 50,
+    padding: 10,
+  },
+  inputButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 5,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#ddd',
+    borderRadius: 5,
+    marginHorizontal: 5,
+    height: 50,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  result: {
+    height: 20,
+    marginVertical: 2,
   },
 });
---
+++ example/src/utils.ts
@@ -0,0 +1,18 @@
+export const PerformanceNow =
+  // @ts-ignore
+  (global.performance && global.performance.now) ||
+  // @ts-ignore
+  global.performanceNow ||
+  // @ts-ignore
+  global.nativePerformanceNow ||
+  (() => {
+    try {
+      var now = require('fbjs/lib/performanceNow');
+    } finally {
+      return now;
+    }
+  })();
+
+const fixed = (n: number) => (Math.trunc(n) === n ? n + '' : n.toFixed(3));
+
+export const timeDelta = (start: number, end: number) => fixed(end - start);
